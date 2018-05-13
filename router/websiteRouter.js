87bc6fdc03582b2075923145d2fa92553f8449a3
zhaojingjing <guzhao593@163.com>
2018-05-13 21:48:37 +0800

    删除网址接口调试完成

--- router/websiteRouter.js
+++ router/websiteRouter.js
@@ -3,5 +3,8 @@ module.exports = {
     app.get('/get-website', (req, res) => {
       db.select(`select * from web where class = '${req.query.class}'`, (data) => res.send(data))
     })
+    app.delete('/delete-website', (req, res) => {
+      db.select(`delete from web where id = '${req.query.id}'`, (data) => res.send(data))
+    })
   }
 }
