25bef2dc3d112fb2aaa51e45d27030260b5be773
zhaojingjing <guzhao593@163.com>
2018-05-13 21:37:07 +0800

    install supervisor 实现热更新;完成获取网址接口

--- router/index.js
+++ router/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
-    if(req.method=="OPTIONS") {
+    if(req.method === "OPTIONS") {
         res.sendStatus(200);/*让options请求快速返回*/
     } else{
         next();
--- router/websiteRouter.js
+++ router/websiteRouter.js
@@ -1,7 +1,7 @@
 module.exports = {
   website: (app, db) => {
     app.get('/get-website', (req, res) => {
-      db.select(`select * from web`, (data) => res.send(data))
+      db.select(`select * from web where class = '${req.query.class}'`, (data) => res.send(data))
     })
   }
 }
